21ca15d760720124e7f9ed754273b41090143c77
Jordan Miller <0jordanmiller@gmail.com>
2020-07-14 15:34:17 -0400

    game works

--- mastermind.rb
+++ mastermind.rb
@@ -1,3 +1,5 @@
+require 'colorize'
+
 def colors
   %w[R G B Y W P]
 end
@@ -24,37 +26,91 @@ def breaker_guess
   input = validate_regex(input)
 end
 
-# def feedback_red(code_maker, code_breaker, pegs)
-#     code_maker.chars.each.with_index do |maker_letter, i|
-#     if maker_letter == code_breaker[i] 
-#       red_accounted[i] = true
-#       pegs[i] = 'red'
-#     end
-#   end
-# end
+def randomize_pegs
+  arr = []
+  4.times  {|i| arr.push(i) }
+  arr.shuffle
+end
+
+def color_switch(color, circle_size)
+  if circle_size == true
+    circle = '⬤'
+  else
+    circle = '●'
+  end
+  case color
+    when 'R'
+      color = circle.red
+    when 'G'
+      color = circle.green
+    when 'B'
+      color = circle.blue
+    when 'W'
+      color = circle.white
+    when 'Y'
+      color = circle.yellow
+    when 'P'
+      color = circle.magenta
+    when nil
+      color = '○'
+  end
+end
+
+def print_board(colors, peg_order)
+  print "Guesses: \n"
+    i = 0
+    j = 0
+    while  j < 4
+        while i < colors.length
+        color = color_switch(colors[i][0][j], true)
+        print color
+        print '            '
+        i += 1
+          if i == colors.length
+            puts ''
+          end
+        end
+    i = 0
+    j += 1
+  end
+  print "\nPegs: \n"
+
+    k = 0
+    l = 0
+    while  l < 4
+        while k < colors.length
+          peg = color_switch(colors[k][1][peg_order[l]], false)
+          print peg
+          print '            '
+          if k + 1 == colors.length
+            puts ''
+          end
+        k += 1
+        end
+    k = 0
+    l += 1
+  end
+end
 
 def feedback(code_maker, code_breaker)
   red_accounted = [false, false, false, false]
   white_accounted = [false, false, false, false]
   pegs = {}
 
-
   # red
   code_maker.chars.each.with_index do |maker_letter, i|
     if maker_letter == code_breaker[i] 
       red_accounted[i] = true
-      pegs[i] = 'red'
+      pegs[i] = 'R'
     end
   end
 
-    # white
+  # white
   code_maker.chars.each.with_index do |maker_letter, i|
     code_breaker.chars.each.with_index do |breaker_letter, j|
-
       if maker_letter == breaker_letter && red_accounted[i] == false && white_accounted[j] == false && red_accounted[j] == false
         white_accounted[j] = true
-        white += 1
-        pegs[i] = 'white'
+        pegs[j] = 'W'
         break
       end
     end
@@ -62,29 +118,25 @@ def feedback(code_maker, code_breaker)
   pegs
 end
 
-
-def check_winner(code_maker, code_breaker)
-  if code_maker == code_breaker
-    true
-  else
-    feedback(code_maker, code_breaker)
-  end
-end
-
 def start_game
+  peg_order = randomize_pegs
   winner = false
   puts 'Try and break the code. Input four colors, Red, Green, Blue, Yellow, White, Purple. Sample input: RGBWY. '
   guesses = []
-  # computer_code = computer_code(colors)
-  # computer_code.chars.each {|code| print code}
+  # maker_code = computer_code(colors)
+  # maker_code.chars.each {|code| print code}
   print "PWRY"
   turn = 0
-  while turn < 10 && guesses[-1] != true
+  while turn < 10 && winner == false
     print "\nPlease guess: "
     breaker_guess = breaker_guess()
-    guesses.push(breaker_guess)
-    guesses.push(check_winner("PWRY", breaker_guess))
-    # p guesses
+    winner = "PWRY" == breaker_guess
+    if winner == true
+      break
+    end
+    guesses.push([breaker_guess, feedback("PWRY", breaker_guess)])
+    print_board(guesses, peg_order)
+    p guesses
     turn += 1
   end
   print 'Winner!' if winner == true
--- tempCodeRunnerFile.rb
+++ tempCodeRunnerFile.rb
@@ -1,1 +1,2 @@
-j
+  # maker_code = computer_code(colors)
+  # maker_code.chars.each {|code| print code}
